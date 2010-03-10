d1615755a169299273ebf0ed7a8f3022bcb3cbf9
keisuke.oohashi <keisuke.oohashi@dd4cad5e-2b9a-11df-ae7a-2764ac86b46b>
2010-03-10 14:48:41 +0000

    Clearなどを設定

--- t2-gae-slim3-mobylet/src/main/java/stk/web/gae/plugin/Slim3Plugin.java
+++ t2-gae-slim3-mobylet/src/main/java/stk/web/gae/plugin/Slim3Plugin.java
@@ -7,8 +7,8 @@ import org.slim3.controller.ControllerConstants;
 import org.slim3.controller.validator.Errors;
 import org.slim3.util.ApplicationMessage;
 import org.slim3.util.RequestLocator;
+import org.slim3.util.ResponseLocator;
 import org.t2framework.t2.plugin.AbstractPlugin;
-
 
 /**
  * slim3の各種機能を利用するためのPlugin
@@ -17,14 +17,55 @@ import org.t2framework.t2.plugin.AbstractPlugin;
  */
 public class Slim3Plugin extends AbstractPlugin {
 
+	/**
+	 * slim3のコントローラクラスの代わりにRequest、Response、Errorsの設定を行います。
+	 *
+	 * @param request Request
+	 * @param response Response
+	 *
+	 * @see org.slim3.controller.FrontController
+	 * @see org.t2framework.t2.plugin.AbstractPlugin#beginRequestProcessing(javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse)
+	 *
+	 */
 	@Override
 	public void beginRequestProcessing(HttpServletRequest request, HttpServletResponse response) {
+
 		RequestLocator.set(request);
+
+		ResponseLocator.set(response);
 
 		ApplicationMessage.setBundle(ControllerConstants.DEFAULT_LOCALIZATION_CONTEXT, request.getLocale());
 
-		request.setAttribute(ControllerConstants.ERRORS_KEY, new Errors());
+		Errors errors = (Errors) request.getAttribute(ControllerConstants.ERRORS_KEY);
+
+		if (errors == null) {
+
+			errors = new Errors();
+
+			request.setAttribute(ControllerConstants.ERRORS_KEY, errors);
+		}
 
 		super.beginRequestProcessing(request, response);
+	}
+
+	/**
+	 * slim3のコントローラクラスの代わりにRequest、Response、Errorsのクリアを行います。
+	 *
+	 * @param request Request
+	 * @param response Response
+	 *
+	 * @see org.slim3.controller.FrontController
+	 * @see org.t2framework.t2.plugin.AbstractPlugin#endRequestProcessing(javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse)
+	 */
+	@Override
+	public void endRequestProcessing(HttpServletRequest request, HttpServletResponse response) {
+
+		ApplicationMessage.clearBundle();
+
+		ResponseLocator.set(response);
+
+		RequestLocator.set(request);
+
+		super.endRequestProcessing(request, response);
 	}
 }
